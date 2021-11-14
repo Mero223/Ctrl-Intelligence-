baa28396f6dedf4b1abe0f6c0b62e9eae6959c8c
technologeve <eve.nicole@icloud.com>
2021-11-14 13:51:28 +0000

    Add cook randomiser and content

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for
 from select_story import select_story
 from select_nature import select_nature
 from select_create import select_create
+from select_cook import select_cook
 
 app = Flask(__name__)
 
@@ -26,30 +27,31 @@ def confirmation():
     return render_template('confirmation.html')
 
 @app.route("/meditate")
-def meditate():  # put application's code here
+def meditate():  
     return render_template("meditate.html")
 
 @app.route("/nature")
-def nature():  # put application's code here
+def nature():  
     selected_suggestion = select_nature()
     return render_template("nature.html", walk=selected_suggestion)
 
 @app.route("/create")
-def create():  # put application's code here
+def create():  
     creative_activity = select_create()
     return render_template("create.html", activity=creative_activity)
 
 @app.route("/cook")
-def cook():  # put application's code here
-    return render_template("cook.html")
+def cook():  
+    recipe_rec = select_cook()
+    return render_template("cook.html", recommendation=recipe_rec)
 
 @app.route("/read")
-def read():  # put application's code here
+def read():  
     selected_story_title, selected_story = select_story()
     return render_template("read.html", title=selected_story_title, story=selected_story)
 
 @app.route("/random")
-def random():  # put application's code here
+def random():  
     return render_template("random.html")
 
 if __name__ == '__main__':
